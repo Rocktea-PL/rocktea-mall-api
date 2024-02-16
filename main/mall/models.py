4ac84bdf644bb058b14dc1db3d2d280f2e8e08bc
Israel Abiona <adeyanjuabiona69@gmail.com>
2024-02-16 17:44:38 +0100

    Domain Name Fix

--- main/mall/models.py
+++ main/mall/models.py
@@ -135,7 +135,7 @@ class Store(models.Model):
    cover_image = models.FileField(storage=RawMediaCloudinaryStorage, null=True)
    year_of_establishment = models.DateField(validators=[YearValidator], null=True)
    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
-   domain_name = models.CharField(max_length=15, null=True, unique=True)
+   domain_name = models.CharField(max_length=100, null=True, unique=True)
    
    temp_id = models.CharField(max_length=10, unique=True, null=True)
    completed = models.BooleanField(default=False)
