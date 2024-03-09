1d478b76388646bb5a9b52fbd52fa99b762f12ef
Israel Abiona <adeyanjuabiona69@gmail.com>
2024-03-09 14:14:09 +0100

    read

---
+++ main/workshop/sales_performance_indicator.py
@@ -0,0 +1,18 @@
+from mall.models import Store
+from order.models import StoreOrder, OrderItems
+
+
+class SalesPerformanceIndicator:
+   def __init__(self):
+      pass
+   
+   def total_sale(self, item):
+      pass
+   
+   def time_since_product_added(self, item, timesince):
+      pass
+   
+   def freq_of_sale(self, item, time_since):
+      pass
+   
+   
