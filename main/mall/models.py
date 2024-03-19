b00b1d750e107f9aa0ac09208d1470bb4a08fd1e
Israel Abiona <adeyanjuabiona69@gmail.com>
2024-03-19 09:21:00 +0100

    ready

--- main/mall/models.py
+++ main/mall/models.py
@@ -405,6 +405,6 @@ class PromoPlans(models.Model):
       
       
 class BuyerBehaviour(models.Model):
-   question = models.CharField(max_length=200, unique=True)
+   question = models.CharField(max_length=200, unique=True, default="How satisfied are you with our services ?")
    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, limit_choices_to={"is_consumer":True})
-   answer = models.TextField()
+   answer = models.IntegerField(null=True)
