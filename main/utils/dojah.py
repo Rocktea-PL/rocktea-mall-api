35dd26be287e69b3e8557ba8c60771fab2c96243
KIngsimeone <simeoneumoh@outlook.com>
2024-07-12 19:41:47 +0100

    verify_drivers_licence

--- main/utils/dojah.py
+++ main/utils/dojah.py
@@ -50,7 +50,7 @@ def verify_nin(nin):
 
         # Set the query parameters
         params = {
-            "bvn": nin
+            "nin": nin
         }
 
         response = requests.get(url, headers=headers, params=params)
@@ -65,3 +65,32 @@ def verify_nin(nin):
         return None
 
 
+def verify_drivers_licence(license_number):
+    try:
+        url = "https://api.dojah.io/api/v1/kyc/dl"
+        
+        # Fetch the API key from environment variables
+        api_key = os.getenv('DOJAH_API_KEY')
+        app_id = os.getenv('DOJAH_APP_ID')
+
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_key,
+            "AppId": app_id
+        }
+
+        # Set the query parameters
+        params = {
+            "license_number": license_number
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        
+        print(response.text)
+        
+        return response
+
+    except Exception as e:
+        logger.error('verify_drivers_licence@error')
+        logger.error(e)
+        return None
