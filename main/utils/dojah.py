883c70c70989fc6341ef07f6ec941c5109235758
KIngsimeone <simeoneumoh@outlook.com>
2024-07-13 07:06:56 +0100

    verify_voter_id

--- main/utils/dojah.py
+++ main/utils/dojah.py
@@ -22,7 +22,11 @@ def verify_bvn(bvn):
             "AppId": app_id
         }
 
-        response = requests.get(url, headers=headers)
+        params = {
+            "bvn": bvn
+        }
+
+        response = requests.get(url, headers=headers, params=params)
         
         print(response.text)
         
@@ -92,5 +96,36 @@ def verify_drivers_licence(license_number):
 
     except Exception as e:
         logger.error('verify_drivers_licence@error')
+        logger.error(e)
+        return None
+    
+
+def verify_voter_id(vin):
+    try:
+        url = "https://api.dojah.io/api/v1/kyc/vin"
+        
+        # Fetch the API key from environment variables
+        api_key = os.getenv('DOJAH_API_KEY')
+        app_id = os.getenv('DOJAH_APP_ID')
+
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_key,
+            "AppId": app_id
+        }
+
+        # Set the query parameters
+        params = {
+            "vin": vin
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        
+        print(response.text)
+        
+        return response
+
+    except Exception as e:
+        logger.error('verify_voter_id@error')
         logger.error(e)
         return None
