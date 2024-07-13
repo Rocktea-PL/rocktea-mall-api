5e032f3090da1bc7ed2b4a5159c452a46f94dbe5
KIngsimeone <simeoneumoh@outlook.com>
2024-07-13 07:11:15 +0100

    verify_international_passport

--- main/utils/dojah.py
+++ main/utils/dojah.py
@@ -11,7 +11,7 @@ def verify_bvn(bvn):
         import requests
 
         url = "https://api.dojah.io/api/v1/kyc/bvn/full"
-        
+
         # Fetch the API key from environment variables
         api_key = os.getenv('DOJAH_API_KEY')
         app_id = os.getenv('DOJAH_APP_ID')
@@ -27,9 +27,9 @@ def verify_bvn(bvn):
         }
 
         response = requests.get(url, headers=headers, params=params)
-        
+
         print(response.text)
-        
+
         return response
 
     except Exception as e:
@@ -41,7 +41,7 @@ def verify_bvn(bvn):
 def verify_nin(nin):
     try:
         url = "https://api.dojah.io/api/v1/kyc/nin"
-        
+
         # Fetch the API key from environment variables
         api_key = os.getenv('DOJAH_API_KEY')
         app_id = os.getenv('DOJAH_APP_ID')
@@ -58,9 +58,9 @@ def verify_nin(nin):
         }
 
         response = requests.get(url, headers=headers, params=params)
-        
+
         print(response.text)
-        
+
         return response
 
     except Exception as e:
@@ -72,7 +72,7 @@ def verify_nin(nin):
 def verify_drivers_licence(license_number):
     try:
         url = "https://api.dojah.io/api/v1/kyc/dl"
-        
+
         # Fetch the API key from environment variables
         api_key = os.getenv('DOJAH_API_KEY')
         app_id = os.getenv('DOJAH_APP_ID')
@@ -89,21 +89,21 @@ def verify_drivers_licence(license_number):
         }
 
         response = requests.get(url, headers=headers, params=params)
-        
+
         print(response.text)
-        
+
         return response
 
     except Exception as e:
         logger.error('verify_drivers_licence@error')
         logger.error(e)
         return None
-    
+
 
 def verify_voter_id(vin):
     try:
         url = "https://api.dojah.io/api/v1/kyc/vin"
-        
+
         # Fetch the API key from environment variables
         api_key = os.getenv('DOJAH_API_KEY')
         app_id = os.getenv('DOJAH_APP_ID')
@@ -120,9 +120,41 @@ def verify_voter_id(vin):
         }
 
         response = requests.get(url, headers=headers, params=params)
-        
+
         print(response.text)
-        
+
+        return response
+
+    except Exception as e:
+        logger.error('verify_voter_id@error')
+        logger.error(e)
+        return None
+
+
+def verify_international_passport(passport_number, surname):
+    try:
+        url = "https://api.dojah.io/api/v1/kyc/passport"
+
+        # Fetch the API key from environment variables
+        api_key = os.getenv('DOJAH_API_KEY')
+        app_id = os.getenv('DOJAH_APP_ID')
+
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_key,
+            "AppId": app_id
+        }
+
+        # Set the query parameters
+        params = {
+            "passport_number": passport_number,
+            "surname": surname
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+
+        print(response.text)
+
         return response
 
     except Exception as e:
