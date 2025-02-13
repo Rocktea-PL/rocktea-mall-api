4356fe0d1d137e43418fdec195c17c7e79919efa
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2025-02-13 12:15:01 +0100

    fix: fix error with adding to cart

--- main/order/models.py
+++ main/order/models.py
@@ -151,7 +151,7 @@ class CartItem(models.Model):
    price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00, null=True)
    
    def __str__(self):
-      return self.cart.id
+    return str(self.cart.id)  # Convert to string
 
 class PaystackWebhook(models.Model):
    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='owner')
