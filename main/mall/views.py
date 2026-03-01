04effc39857be9cb6510086c97c2760f313b6456
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2026-03-01 17:24:08 +0100

    fix: remove domain validation for store products when mall parameter is provided

--- main/mall/views.py
+++ main/mall/views.py
@@ -499,16 +499,6 @@ class ProductViewSet(viewsets.ModelViewSet):
                pass
       
       if store:
-         # Validate store belongs to current domain
-         if store.domain_name:
-            from .domain_utils import extract_primary_domain
-            store_platform = extract_primary_domain(store.domain_name)
-            request_host = self.request.get_host()
-            request_platform = extract_primary_domain(request_host)
-            
-            if store_platform != request_platform:
-               return Product.objects.none()
-         
          # Get products that are in this store's marketplace and available
          return Product.objects.filter(
             id__in=StoreProductPricing.objects.filter(store=store).values_list('product_id', flat=True),
